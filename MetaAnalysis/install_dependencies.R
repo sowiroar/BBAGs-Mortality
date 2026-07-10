82fcc7c583fab9349df523eb9c4d90b992e363b6
Ernesto Guevara Navarro <ernesto.guevaran@autonoma.edu.co>
2026-07-10 17:31:04 -0500

    feat: add R dependency installation script and forest plot data files for meta-analysis

--- MetaAnalysis/install_dependencies.R
+++ MetaAnalysis/install_dependencies.R
@@ -2,5 +2,11 @@
 # Run this script before executing any of the meta_analys_*.R files.
 
 options(timeout = 600) # Increase download timeout for large packages
-install.packages(c("meta", "dplyr", "writexl"), dependencies = TRUE, repos = "http://cran.us.r-project.org")
+
+# Install general dependencies and remotes (to install historical versions)
+install.packages(c("remotes", "dplyr", "writexl"), dependencies = TRUE, repos = "http://cran.us.r-project.org")
+
+# Install the exact version of the 'meta' package used for the paper (v7.0-0 supports "BMJ" layout and legacy labels)
+remotes::install_version("meta", version = "7.0-0", repos = "http://cran.us.r-project.org", upgrade = "never")
+
 cat("Dependencies installed successfully!\n")
